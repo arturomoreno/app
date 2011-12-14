be129797c5c183877e5073b94958b5619e0deae4
arturomoreno <artu_dee2@hotmail.com>
2011-12-14 10:36:34 -0800

    Pushing new changes

--- source/app/web/core/FrontController.cs
+++ source/app/web/core/FrontController.cs
@@ -2,6 +2,12 @@
 {
   public class FrontController : IProcessRequests
   {
+  	IFindCommands command_registry;
+
+	  public FrontController(IFindCommands command_registry)
+	  {
+	  	this.command_registry = command_registry;
+	  }
     public void process(IProvideDetailsToCommands request)
     {
       throw new System.NotImplementedException();
