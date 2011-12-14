77506d15f56405439ef08c7b94e2de4e75f3ecdf
arturomoreno <artu_dee2@hotmail.com>
2011-12-14 11:11:47 -0800

    Pushing new changes

--- source/app/web/core/CommandRegistry.cs
+++ source/app/web/core/CommandRegistry.cs
@@ -6,15 +6,18 @@ namespace app.web.core
   public class CommandRegistry : IFindCommands
   {
     IEnumerable<IProcessASingleRequest> commands;
+  	IProcessASingleRequest special_case;
 
-    public CommandRegistry(IEnumerable<IProcessASingleRequest> commands)
+    public CommandRegistry(IEnumerable<IProcessASingleRequest> commands, IProcessASingleRequest special_case)
     {
       this.commands = commands;
+    	this.special_case = special_case;
     }
 
     public IProcessASingleRequest get_the_command_that_can_process(IProvideDetailsToCommands request)
     {
-      return commands.First(x => x.can_process(request));
+      //return commands.First(x => x.can_process(request));
+    	return commands.FirstOrDefault(x => x.can_process(request)) ?? special_case;
     }
   }
 }
