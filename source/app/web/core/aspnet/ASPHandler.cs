dff3247795c86d1165401ddaee7487404497d779
arturomoreno <artu_dee2@hotmail.com>
2011-12-14 10:15:06 -0800

    Pushing new changes

--- source/app/web/core/aspnet/ASPHandler.cs
+++ source/app/web/core/aspnet/ASPHandler.cs
@@ -6,6 +6,7 @@ namespace app.web.core.aspnet
   public class ASPHandler : IHttpHandler
   {
     IProcessRequests front_controller;
+  	private ICreateControllerRequests request_factory;
 
     public ASPHandler(IProcessRequests front_controller)
     {
@@ -15,7 +16,7 @@ namespace app.web.core.aspnet
     public void ProcessRequest(HttpContext context)
     {
 
-      throw new NotImplementedException();
+      front_controller.process(request_factory.create_request_from(context));
     }
 
     public bool IsReusable
