57095bb27e7c4ac4d7d10e8901aad4b35adb5f6e
arturomoreno <artu_dee2@hotmail.com>
2011-12-14 14:54:58 -0800

    Pushing new changes

--- source/app.specs/ViewTheProductsInADepartmentSpecs.cs
+++ source/app.specs/ViewTheProductsInADepartmentSpecs.cs
@@ -1,4 +1,5 @@
-﻿using Machine.Specifications;
+﻿using System.Collections.Generic;
+using Machine.Specifications;
 using app.web.application.catalogbrowsing;
 using app.web.core;
 using developwithpassion.specifications.rhinomocks;
@@ -15,7 +16,28 @@ namespace app.specs
 
     public class when_run : concern
     {
-      It first_observation = () => 
+    	private Establish c = () =>
+		{
+    		display_engine = depends.on<IDisplayReports>();
+    		request = fake.an<IProvideDetailsToCommands>();
+
+		};
+
+    	private Because b = () =>
+    	{
+			sut.pr
+    	};
+
+		It should_display_products_in_a_department = () =>
+    	    display_engine.recieved(x => display(sub_department_products));
+
+
+		static IFindDepartments department_repository;
+		static IProvideDetailsToCommands request;
+		static IDisplayReports display_engine;
+		static DepartmentItem the_main_department;
+		static IEnumerable<DepartmentItem> the_sub_departments;
+
     }
   }
 }
