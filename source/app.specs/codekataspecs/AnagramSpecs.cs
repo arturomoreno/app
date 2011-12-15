f502e2031ea2dcd9a73567c22512e8501f7f7bdd
arturomoreno <artu_dee2@hotmail.com>
2011-12-15 08:43:47 -0800

    Pushing new changes

---
+++ source/app.specs/codekataspecs/AnagramSpecs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using developwithpassion.specifications.extensions;
+using developwithpassion.specifications.rhinomocks;
+
+namespace app.specs.codekataspecs
+{
+	[Subject(typeof(Anagaram))]
+	public class AnagramSpecs
+	{
+
+		public class when_reading_input_words
+		{
+			Establish c = () =>
+			{
+
+			};
+
+			Because b = () =>
+			{
+
+			};
+
+			It should_receive_file ;
+
+			It should_iterate_through_each_line;
+
+			It should_iterate_through_each_letter_of_each_line;
+		}
+
+		
+	}
+
+}
