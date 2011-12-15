d020f33b50150b687c500edde8632faa42e52134
arturomoreno <artu_dee2@hotmail.com>
2011-12-14 19:10:35 -0800

    Pushing new changes

--- source/app/web/core/aspnet/WebFormDisplayEngine.cs
+++ source/app/web/core/aspnet/WebFormDisplayEngine.cs
@@ -1,17 +1,23 @@
-﻿namespace app.web.core.aspnet
+﻿using System.Web;
+
+
+namespace app.web.core.aspnet
 {
   public class WebFormDisplayEngine : IDisplayReports
   {
     ICreateWebFormViewsToDisplayReports view_factory;
+  	private HttpContext the_current_context;
 
-    public WebFormDisplayEngine(ICreateWebFormViewsToDisplayReports view_factory)
+    public WebFormDisplayEngine(ICreateWebFormViewsToDisplayReports view_factory, HttpContext the_current_context)
     {
       this.view_factory = view_factory;
+    	this.the_current_context = the_current_context;
     }
 
     public void display<Report>(Report report)
     {
       view_factory.create_view_that_can_display(report);
+    	view_factory.create_view_that_can_display(report).ProcessRequest(the_current_context);
     }
   }
 }
